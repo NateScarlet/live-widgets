b5a82eab4c29af6e1aeca7484d94ed04989b1785
NateScarlet <NateScarlet@Gmail.com>
2023-10-29 18:29:15 +0800

    fix(clock): not centered in obs

--- src/Clock.vue
+++ src/Clock.vue
@@ -1,40 +1,35 @@
 <template>
-  <svg
-    viewBox="0 0 145 20"
-    xmlns="http://www.w3.org/2000/svg"
-    class="bg-black bg-opacity-50 w-full"
-  >
-    <text x="5" y="14" fill="white" style="font: 12px sans-serif">{{ nowText }}</text>
-  </svg>
+  <div class="bg-black bg-opacity-50 w-screen h-screen flex flex-center p-2">
+    <svg viewBox="0 0 130 20" xmlns="http://www.w3.org/2000/svg" class="w-full">
+      <text x="5" y="14" fill="white" font-size="10">
+        {{ nowText }}
+      </text>
+    </svg>
+  </div>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, onMounted, onUnmounted, ref } from "vue";
+import { computed, onUnmounted, ref } from "vue";
+</script>
 
-export default defineComponent({
-  setup() {
-    const el = ref<HTMLElement | undefined>();
-    const now = ref(new Date());
-    const timer = setInterval(() => {
-      now.value = new Date();
-    }, 100);
-    onUnmounted(() => {
-      clearInterval(timer);
-    });
-    const nowText = computed(() => {
-      return now.value.toLocaleString(undefined, {
-        hour12: false,
-        year: "numeric",
-        month: "numeric",
-        day: "numeric",
-        weekday: "short",
-        hour: "2-digit",
-        minute: "2-digit",
-        second: "2-digit",
-      });
-    });
-
-    return { now, nowText };
-  },
+<script setup lang="ts">
+const now = ref(new Date());
+const timer = setInterval(() => {
+  now.value = new Date();
+}, 100);
+onUnmounted(() => {
+  clearInterval(timer);
+});
+const nowText = computed(() => {
+  return now.value.toLocaleString(undefined, {
+    hour12: false,
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+    weekday: "short",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  });
 });
 </script>
